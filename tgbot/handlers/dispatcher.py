e4e853e590e7b24a5594ef2de7888c0a8da42ba5
Anton Kostin <masguit42@users.noreply.github.com>
2021-08-28 13:51:33 +0700

    Fix methods

--- tgbot/handlers/dispatcher.py
+++ tgbot/handlers/dispatcher.py
@@ -33,7 +33,7 @@ def setup_dispatcher(dp):
     dp.add_handler(MessageHandler(Filters.regex('^(\w|\.)+@phystech\.edu$'), handlers.wait_for_email))
     dp.add_handler(MessageHandler(Filters.regex('^[A-Z0-9]{5}$'), handlers.wait_for_code))
     dp.add_handler(Filters.text, send_invitation)
-    
+
 
     dp.add_error_handler(error.send_stacktrace_to_tg_chat)
 

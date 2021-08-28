ca4e21e91b55da08a6783d914c145847ee4a99f9
Anton Kostin <masguit42@users.noreply.github.com>
2021-08-28 13:38:16 +0700

    Add handlers

--- tgbot/handlers/dispatcher.py
+++ tgbot/handlers/dispatcher.py
@@ -22,14 +22,18 @@ def setup_dispatcher(dp):
 
     # Broadcast 
     dp.add_handler(CommandHandler("start", handlers.main_menu))
-
+    dp.add_handler(CommandHandler("chats", handlers.show_chats))
+    dp.add_handler(CommandHandler("services", handlers.services))
+    dp.add_handler(CommandHandler("help", help_menu))
 
     dp.add_handler(MessageHandler(Filters.regex('^(Добавиться в чат)$'), handlers.add_to_chat))
     dp.add_handler(MessageHandler(Filters.regex('^(Показать чаты)$'), handlers.show_chats))
     dp.add_handler(MessageHandler(Filters.regex('^(Показать сервисы)$'), handlers.show_services))
 
-    dp.add_handler(MessageHandler(Filters.regex('^(\w|\.)+@phystech\.edu$'), handlers.add_to_chat))
-
+    dp.add_handler(MessageHandler(Filters.regex('^(\w|\.)+@phystech\.edu$'), handlers.wait_for_email))
+    dp.add_handler(MessageHandler(Filters.regex('^[A-Z0-9]{5}$'), handlers.wait_for_code))
+    dp.add_handler(Filters.text, send_invitation)
+    
 
     dp.add_error_handler(error.send_stacktrace_to_tg_chat)
 
